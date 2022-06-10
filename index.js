e7aa80baf834160cf2fb8d06882591c51646418a
JMAC <jmac@macbook-pro.myfiosgateway.com>
2022-06-09 23:14:47 -0400

    Second commit

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const Discord = require("discord.js")
 
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
         "GUILD_MESSAGES",
+        "GUILD_MEMBERS",
     ]
 })
 
@@ -20,4 +23,14 @@ client.on("messageCreate", (message) => {
 
 })
 
-client.login(TOKEN)
+const welcomeChannelId = "984136771598155836"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}>  Welcome to the server!`,
+        files: [img]
+    })
+})
+
+client.login(process.env.TOKEN)
